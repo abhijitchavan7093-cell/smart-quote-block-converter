42e3df8b1d1b4b93f3eb966a2e1d010b7d741fdf
abhijitchavan7093-cell <abhijitchavan7093@gmail.com>
2026-07-06 02:46:26 +0530

    Update class-loader.php

--- includes/class-loader.php
+++ includes/class-loader.php
@@ -11,11 +11,11 @@ class SQBC_Loader {
 		require_once SQBC_PLUGIN_PATH . 'includes/class-admin.php';
 		require_once SQBC_PLUGIN_PATH . 'includes/class-scanner.php';
 		require_once SQBC_PLUGIN_PATH . 'includes/class-detector.php';
+		require_once SQBC_PLUGIN_PATH . 'includes/class-converter.php';
+		require_once SQBC_PLUGIN_PATH . 'includes/class-helper.php';
 
 		if ( is_admin() ) {
 			new SQBC_Admin();
 		}
-
 	}
-
 }
