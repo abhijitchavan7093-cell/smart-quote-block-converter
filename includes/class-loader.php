fe1af07c33ac8d22f1e9f66b45ca2febbbefe208
abhijitchavan7093-cell <abhijitchavan7093@gmail.com>
2026-07-06 02:27:19 +0530

    Update class-loader.php

--- includes/class-loader.php
+++ includes/class-loader.php
@@ -1,1 +1,27 @@
+<?php
+/**
+ * Plugin Loader
+ *
+ * @package SmartQuoteBlockConverter
+ */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class SQBC_Loader {
+
+	/**
+	 * Initialize plugin.
+	 */
+	public function init() {
+
+		// Admin Area
+		if ( is_admin() ) {
+			require_once SQBC_PLUGIN_PATH . 'includes/class-admin.php';
+			new SQBC_Admin();
+		}
+
+	}
+
+}
